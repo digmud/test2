7a645ee6a9cf8100f326be46d607afbb791c87f8
digmud <264201116+digmud@users.noreply.github.com>
2026-03-01 12:31:58 +0100

    built with bundle... no prod env

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1772208320',resources: [
+    cacheName: 'chirpy-1772364625',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
